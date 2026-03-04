e2d27f00b206750a382d86d70f2921f3c7422523
Janko0311 <jankocheng0311@gmail.com>
2026-03-04 10:20:05 -0500

    Delete Smart course companion student(1.0.1 Beta) directory

--- Smart course companion student(1.0.1 Beta)/settings.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("theme-toggle").addEventListener("change", function() {
-    if (this.checked) {
-        document.body.classList.add("light-mode");
-        document.body.classList.remove("dark-mode");
-    } else {
-        document.body.classList.add("dark-mode");
-        document.body.classList.remove("light-mode");
-    }
-});
